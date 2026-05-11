779995eba2e48bac525541ce286dcb173b20186a
TT Pixel Development <tobauntat@gmail.com>
2026-05-11 23:37:40 +0200

    Revert "Analytics added"
    This reverts commit 2f657d5629d556bf01d36c9addb2922aab40a4ad.

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
-import { Analytics } from "@vercel/analytics/next"
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Analytics/>
     <App />
   </StrictMode>,
 );
